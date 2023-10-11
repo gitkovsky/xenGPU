db4327e92cabaa787b1e0ddf97e106d565e6655b
gitkovsky <77162920+gitkovsky@users.noreply.github.com>
2023-10-11 21:47:46 +0200

    Create vastsm86.sh

---
+++ vastsm86.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Update package list and install necessary packages
+
+sudo apt update
+sleep 5
+echo "---------------------------"
+
+sudo apt -y install ocl-icd-opencl-dev
+sleep 5
+echo "---------------------------"
+
+sudo apt -y install nano
+sleep 5
+echo "---------------------------"
+
+sudo apt -y install cmake
+sleep 5
+echo "---------------------------"
+
+sudo apt -y install python3-pip
+sleep 5
+echo "---------------------------"
+
+# Clone the repository and build the project
+git clone https://github.com/shanhaicoder/XENGPUMiner.git
+sleep 5
+echo "---------------------------"
+
+cd XENGPUMiner
+sleep 5
+echo "---------------------------"
+
+chmod +x miner.sh
+sleep 5
+echo "---------------------------"
+
+chmod +x build.sh
+sleep 5
+echo "---------------------------"
+
+./build.sh -cuda_arch sm_86
+sleep 5
+echo "---------------------------"
+
+# Update the configuration file
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xE92104Fd9131217326329E71464F24DeD78f745c/g' config.conf
+sleep 5
+echo "---------------------------"
+
+# Install Python requirements
+sudo pip install -U -r requirements.txt
+sleep 5
+echo "---------------------------"
+
+sudo apt install screen
+sleep 5
+echo "---------------------------"
