ea91d9e0f9b8b0c19e6f083b6e12a24f885b79e8
gitkovsky <77162920+gitkovsky@users.noreply.github.com>
2023-10-23 20:06:12 +0200

    Update vastsm89date.sh

--- vastsm89date.sh
+++ vastsm89date.sh
@@ -49,15 +49,15 @@ echo "-------------config.conf address changed--------------"
 sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xE92104Fd9131217326329E71464F24DeD78f745c/g' config.conf
 sleep 1
 
-echo "-------------sed -i 's@BLUE = "\\033\[34m@BLUE = "\\033\[36m@g' miner.py--------------"
+echo "-------------miner.py change BLUE color--------------"
 sed -i 's@BLUE = "\\033\[34m@BLUE = "\\033\[36m@g' miner.py
 sleep 1
 
-echo "-------------sed -i 's@print(f"Mining block {i}...")@print(f"Mining block {i}..." + datetime.now().strftime('\''%Y-%m-%d %H:%M:%S'\''))@g' miner.py--------------"
+echo "-------------miner.py add datetime to Mining block--------------"
 sed -i 's@print(f"Mining block {i}...")@print(f"Mining block {i}..." + datetime.now().strftime('\''%Y-%m-%d %H:%M:%S'\''))@g' miner.py
 sleep 1
 
-echo "-------------sed -i 's@desc=f"{GREEN}Mining{RESET}"@desc=f"{datetime.now().strftime('\''%Y-%m-%d %H:%M:%S'\'')} {GREEN}Mining{RESET}"@g' miner.py--------------"
+echo "-------------miner.py add datetime to Mining--------------"
 sed -i 's@desc=f"{GREEN}Mining{RESET}"@desc=f"{datetime.now().strftime('\''%Y-%m-%d %H:%M:%S'\'')} {GREEN}Mining{RESET}"@g' miner.py
 sleep 1
 
